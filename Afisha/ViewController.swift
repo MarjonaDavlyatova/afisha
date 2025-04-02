9db3be2d7b61389039591b33a2da2ccb3de01c8d
MarjonaDavlyatova <marjona.davlyatova.h@gmail.com>
2025-04-02 15:37:20 +0500

    Initial Commit

---
+++ Afisha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Afisha
+//
+//  Created by Marjona Davlyatova on 02.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
